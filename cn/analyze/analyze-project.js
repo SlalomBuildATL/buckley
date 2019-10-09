e45663053e77121696554e6ec9644d244df2ff88
Raz Ali <raz.ali@slalom.com>
2019-10-09 16:08:17 -0400

    CN search recursively for project settings file

--- cn/analyze/analyze-project.js
+++ cn/analyze/analyze-project.js
@@ -1,31 +1,41 @@
-const { verifyScriptingBackend } = require('./check-scripting-backend');
-const { projectConfig, targets } = require('../cn-remediation-config.json');
-const { readConfigFile } = require('./utils');
+const {verifyScriptingBackend} = require('./check-scripting-backend');
+const {projectConfig, targets} = require('../cn-remediation-config.json');
+const {readConfigFile} = require('./utils');
 const checkScriptingBackend = require('./check-scripting-backend');
-const { verifyAndroidTargetArchitecture } = require('./check-target-architecture');
-const { verifyAbsenceOfFiles } = require('./verify-absence-of-files');
+const {verifyAndroidTargetArchitecture} = require('./check-target-architecture');
+const {verifyAbsenceOfFiles} = require('./verify-absence-of-files');
+const find = require('find');
+const Promise = require('promise');
 
 function getPlayerSettings() {
-    let playerSettings;
-    try {
-        playerSettings = readConfigFile(projectConfig).PlayerSettings;
-    } catch (e) {
-        console.error(chalk.red(`Error: ${e}`));
-        console.error(chalk.red(`Couldn't read ProjectSettings.asset file. Are you in the root directory of the game project?`))
-    }
-    return playerSettings;
+    return new Promise((resolve, reject) => {
+        find.file(projectConfig, './', (files) => {
+            if (files.length) {
+                try {
+                    resolve(readConfigFile(files[0]).PlayerSettings);
+                } catch (e) {
+                    console.error(chalk.red(`Error: ${e}`));
+                    reject('Could not read project config file')
+                }
+            } else {
+                reject('Could not find project config file');
+                console.error(chalk.red(`Could not find ${projectConfig} file. Are you running this from a game directory?`));
+                }
+            }
+        );
+    })
 }
 
 module.exports = {
     analyzeProject: (env) => {
-        const playerSettings = getPlayerSettings();
-
-        verifyScriptingBackend(playerSettings, 'Android', targets.Android);
-        verifyScriptingBackend(playerSettings, 'iOS', targets.Android);
-        verifyAndroidTargetArchitecture(playerSettings, targets.Android);
-        verifyAbsenceOfFiles(/upsight/i);
-        verifyAbsenceOfFiles(/playhaven/i);
-        verifyAbsenceOfFiles(/kochava/i);
-        verifyAbsenceOfFiles(/comscore/i);
+        getPlayerSettings().then(playerSettings => {
+            verifyScriptingBackend(playerSettings, 'Android', targets.Android);
+            verifyScriptingBackend(playerSettings, 'iOS', targets.Android);
+            verifyAndroidTargetArchitecture(playerSettings, targets.Android);
+            verifyAbsenceOfFiles(/upsight/i);
+            verifyAbsenceOfFiles(/playhaven/i);
+            verifyAbsenceOfFiles(/kochava/i);
+            verifyAbsenceOfFiles(/comscore/i);
+        });
     }
 };
