4de7c07f4de34ca75cce587c4504f2116e5f524a
Raz Ali <raz.ali@slalom.com>
2020-04-09 12:09:26 -0400

    Minor cleanup on bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NODE_VERSION=12.16.2
+
 function install_node() {
   set -e
   #install nvm
@@ -10,15 +12,12 @@ function install_node() {
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    #install nvm
     install_nvm_via_homebrew
-    # setup nvm
-    source $(brew --prefix nvm)/nvm.sh
   fi
 
   # setup node
-  nvm install 12.16.2
-  nvm use 12.16.2
+  nvm install $NODE_VERSION
+  nvm use --delete-prefix $NODE_VERSION
   echo "Node.js/NPM was successfully installed via Node Version Manager (NVM). Visit https://github.com/nvm-sh/nvm for more information on how to use NVM."
   set +e
 }
@@ -27,10 +26,12 @@ function install_nvm_via_homebrew() {
   if ! command -v brew; then
     echo "Installing Homebrew Package Manager"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
   fi
   brew update
   brew install nvm
+
+  # setup nvm
+  source $(brew --prefix nvm)/nvm.sh
 }
 
 function install_buckley() {
