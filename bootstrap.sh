86f22eb54ccb2016d3875faf740ef35b7745dc9b
Raz Ali <raz.ali@slalom.com>
2020-04-08 21:39:23 -0400

    Install NVM via homebrew on Macs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
 
 function install_node() {
-    #install nvm
+  #install nvm
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-    
-    # setup nvm
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-    
-    # setup node
-    nvm install 10.13.0
-    nvm use 10.13.0
-    echo "Node.js/NPM was successfully installed via Node Version Manager (NVM). Visit https://github.com/nvm-sh/nvm for more information on how to use NVM."
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    install_nvm_via_homebrew
+  fi
+
+  # setup nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+  # setup node
+  nvm install 10.13.0
+  nvm use 10.13.0
+  echo "Node.js/NPM was successfully installed via Node Version Manager (NVM). Visit https://github.com/nvm-sh/nvm for more information on how to use NVM."
+}
+
+function install_nvm_via_homebrew() {
+  if ! command -v brew; then
+    echo "Installing Homebrew Package Manager"
+    /usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"
+  fi
 }
 
 function install_buckley() {
-    mkdir -p ~/dev
-    cd ~/dev
-    git clone https://github.com/SlalomBuildATL/buckley.git
-    cd buckley
-    npm install -g .
-    echo "Buckley was successfully installed. Please use 'bcli install --tags recommended' to install recommended packages"
+  mkdir -p ~/dev
+  cd ~/dev
+  git clone https://github.com/SlalomBuildATL/buckley.git
+  cd buckley
+  npm install -g .
+  echo "Buckley was successfully installed. Please use 'bcli install --tags recommended' to install recommended packages"
 
 }
 
 #check for npm installation
 if ! command -v npm; then
-    install_node
+  install_node
 fi
 
 if ! command -v bcli; then
-    install_buckley
+  install_buckley
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
