ec1b5e43938d3758e2c4415e2ebb888e8fce533c
Raz Ali <raz.ali@slalom.com>
2020-04-09 17:06:27 -0400

    Remove CN module

--- cn/analyze/cn-analyze.js
+++
@@ -1,7 +0,0 @@
-const cli = require('commander');
-const { analyzeProject } = require('./analyze-project');
-
-cli
-    .option('-v, --verbose', 'verbose')
-    .action(analyzeProject)
-    .parse(process.argv);
