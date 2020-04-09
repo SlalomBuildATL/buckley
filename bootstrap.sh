0e138be0c674210f5d93d5380e33bc716843f28d
Raz Ali <raz.ali@slalom.com>
2020-04-08 21:58:51 -0400

    Fix nvm setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,16 @@ function install_node() {
   #install nvm
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    # setup nvm
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   elif [[ "$OSTYPE" == "darwin"* ]]; then
+    #install nvm
     install_nvm_via_homebrew
+    # setup nvm
+    source $(brew --prefix nvm)/nvm.sh
   fi
-
-  # setup nvm
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
   # setup node
   nvm install 10.13.0
