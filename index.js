645aca3c924c85cf1562aeaecf01cad47ac9a29c
Raz Ali <raz.ali@slalom.com>
2020-04-18 12:21:01 -0400

    Use local version when displaying version as previous command would show the latest verison on npm.js

--- index.js
+++ index.js
@@ -2,22 +2,19 @@
 const cli = require('commander');
 const figlet = require('figlet');
 const { exec }= require('child_process')
+const version = require('./package.json').version;
 
 if (!process.argv.slice(2).length) {
     cli.outputHelp(figletPrint);
 }
 
-
 cli
-    .option('-v, --version', 'show installed version of buckley', showVersion)
+    .option('-v, --version', 'show installed version of buckley', () => console.log(version))
     .command('install', 'install one or more packages to your workstation', {executableFile: 'install/bcli-install'})
     .command('git', 'helper functions related to git', {executableFile: 'git/cli/bcli-git.js'})
     .parse(process.argv);
 
 
-function showVersion() {
-    exec(`npm show buckley version`, null, (err, stdout) => console.log(stdout.trim()))
-}
 
 function figletPrint(text) {
     let header = figlet.textSync('Buckley', {
