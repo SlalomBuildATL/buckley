60bdc1408bcd1af311a847fe5c1e91ed0ec3c5dd
Raz Ali <raz.ali@slalom.com>
2020-04-18 11:44:50 -0400

    Use exec and git command line instead of nodegit to set aliases because nodegit sucks

--- git/configs/configureGit.js
+++ git/configs/configureGit.js
@@ -1,12 +1,15 @@
-const Git = require('nodegit');
 const aliasConfigs = require('./git-config').aliases;
+const chalk = require('chalk');
+const {execSync} = require('child_process')
 
 function configureAliases(env) {
-    return Git.Config.findGlobal()
-        .then(globalConfig => Git.Config.openOndisk(globalConfig)
-            .then(config => aliasConfigs.forEach(aliasConfig =>
-                aliasConfig.aliases.forEach(alias => config.setString(`alias.${alias}`, aliasConfig.action)
-                    .then(() => console.log(`Aliased ${aliasConfig.action} (${aliasConfig.description}) to ${alias}`))))))
+    aliasConfigs.forEach(config => {
+        config.aliases.forEach(alias => {
+            const command = `git config --global alias.${alias} "${config.action}"`;
+            execSync(command);
+            console.log(`Aliased ${config.action} (${config.description}) to ${chalk.bold(alias)}`);
+        });
+    });
 }
 
 module.exports = {
